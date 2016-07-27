a0484915cb6a4e38d768662164131de64c90b5dc
Scottie Crump <scottiecrump@yahoo.com>
2016-07-27 09:24:53 -0400

    can now get search results on keyup

--- app.js
+++ app.js
@@ -4,18 +4,24 @@
 
 var wikiApp = angular.module('wikiApp', [])
 .controller('ApiCtrl', function($scope, $http) {
-  var searchItm = "dog";
-  $http.get("https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+searchItm+"")
-  .then(function(response) {
-      $scope.wikiresults = response.data;
-      for(var i = 0; i < $scope.wikiresults.length; i++) {
-        $('.col-lg-12').prepend('<div class="results bg-primary container" style="margin-top: 1rem; margin-bottom: 1rem;">'+
-         ' <a style="color:white;text-decoration:none;" href="'+$scope.wikiresults[3][i]+'" target="_blank">'+
-         '<h4>'+$scope.wikiresults[1][i]+'</h4>'+
-         '<p>'+$scope.wikiresults[2][i]+'</p>'+
-         '</a>' +
-         '</div>'
-        );
-      }
-  });
+
+  $scope.wikisearch = function() {
+    var searchItm = $('#searchItm').val();
+    //if search value typed in, run the search
+    if(searchItm !== '') {
+    $http.get("https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+searchItm+"")
+    .then(function(response) {
+        $scope.wikiresults = response.data;
+        for(var i = 0; i < $scope.wikiresults[1].length; i++) {
+          $('.col-lg-12').prepend('<div class="results bg-primary container" style="margin-top: 1rem; margin-bottom: 1rem;">'+
+           ' <a style="color:white;text-decoration:none;" href="'+$scope.wikiresults[3][i]+'" target="_blank">'+
+           '<h4>'+$scope.wikiresults[1][i]+'</h4>'+
+           '<p>'+$scope.wikiresults[2][i]+'</p>'+
+           '</a>' +
+           '</div>'
+          );
+        }
+    });
+  } //if
+  };// function
 });
