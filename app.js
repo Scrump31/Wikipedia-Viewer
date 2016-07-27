b59bd92ac3f9b697966527883b65231eb5b38ef5
Scottie Crump <scottiecrump@yahoo.com>
2016-07-27 10:33:36 -0400

    decided to return search results on button click

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ var wikiApp = angular.module('wikiApp', [])
     $http.get("https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+searchItm+"")
     .then(function(response) {
         $scope.wikiresults = response.data;
+        $('.col-lg-12').html('');
         for(var i = 0; i < $scope.wikiresults[1].length; i++) {
+
+          if($scope.wikiresults[1].length < 0) {$('.col-lg-12').prepend('Sorry, no results.');}
+
           $('.col-lg-12').prepend('<div class="results bg-primary container" style="margin-top: 1rem; margin-bottom: 1rem;">'+
            ' <a style="color:white;text-decoration:none;" href="'+$scope.wikiresults[3][i]+'" target="_blank">'+
            '<h4>'+$scope.wikiresults[1][i]+'</h4>'+
@@ -21,7 +25,10 @@ var wikiApp = angular.module('wikiApp', [])
            '</div>'
           );
         }
+    }, function errorCallback(response) {
+          console.log(response);
     });
   } //if
+
   };// function
 });
