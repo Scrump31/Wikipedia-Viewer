f57dfbbbcefd20771f724cf9e56a20403d7211ec
Scottie Crump <scottiecrump@yahoo.com>
2016-07-27 11:13:53 -0400

    added comments

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 (function () {
    'use strict';
 }());
-
 var wikiApp = angular.module('wikiApp', [])
 .controller('ApiCtrl', function($scope, $http) {
-
+  // Runs when search button clicked
   $scope.wikisearch = function() {
     var searchItm = $('#searchItm').val();
-    //if search value typed in, run the search
+    // If search input not empty, run the search
     if(searchItm !== '') {
     $http.get("https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+searchItm+"")
     .then(function(response) {
         $scope.wikiresults = response.data;
+        // Clears out search result area
         $('.col-lg-12').html('');
         for(var i = 0; i < $scope.wikiresults[1].length; i++) {
-
-          if($scope.wikiresults[1].length < 0) {$('.col-lg-12').prepend('Sorry, no results.');}
-
+          // Loads search results into page
           $('.col-lg-12').prepend('<div class="results bg-primary container animated rollIn" style="margin-top: 1rem; margin-bottom: 1rem;">'+
            ' <a style="color:white;text-decoration:none;" href="'+$scope.wikiresults[3][i]+'" target="_blank">'+
            '<h4>'+$scope.wikiresults[1][i]+'</h4>'+
@@ -25,10 +23,10 @@ var wikiApp = angular.module('wikiApp', [])
            '</div>'
           );
         }
+    // Log any errors
     }, function errorCallback(response) {
           console.log(response);
     });
   } //if
-
-  };// function
-});
+};// wikisearch function
+});// api controller
